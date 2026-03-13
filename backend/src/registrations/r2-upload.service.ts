2c5f2b030438a1644a371700697f636190a1d69e
BR2005 <77bharathcyber@gmail.com>
2026-03-13 10:31:40 +0530

    hello

--- backend/src/registrations/r2-upload.service.ts
+++ backend/src/registrations/r2-upload.service.ts
@@ -28,6 +28,7 @@ export class R2UploadService {
     this.client = new S3Client({
       region: 'auto',
       endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+      forcePathStyle: true,
       credentials: {
         accessKeyId,
         secretAccessKey,
