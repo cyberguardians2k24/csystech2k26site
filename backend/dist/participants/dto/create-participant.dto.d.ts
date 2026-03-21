20b7bd1e603e294535b2170f72b46edc0eb302cc
BR2005 <77bharathcyber@gmail.com>
2026-03-21 18:11:58 +0530

    updated DB

--- backend/dist/participants/dto/create-participant.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateParticipantDto {
-    name: string;
-    email: string;
-    phone: string;
-    college: string;
-    teamName?: string;
-}
--- frontend/dist/assets/paymentimage-CxNR9zKi.js
+++
@@ -1,1 +0,0 @@
-const e="/assets/paymentimage-BC8bid5u.jpeg";export{e as p};
